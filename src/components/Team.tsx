2e84bf0401446cc2544cd33ab811659ad51ea21a
Forest Lam Nguyen <sogoten6689@gmail.com>
2023-12-02 03:28:39 +0700

    build code

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -12,8 +12,7 @@ export const Team = (props: TeamProps) => {
         <div className="col-md-8 col-md-offset-2 section-title">
           <h2>Meet the Team</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          Our talented and dedicated team members.
           </p>
         </div>
         <div id="row">
